f69af2089993d63571b27f7ef70cc04308ad80fb
rodongwan <rodongwan@burritowallet.com>
2026-04-15 14:18:54 +0900

    practice coding

--- src/main/java/com/sparta/miniorder/MiniorderApplication.java
+++ src/main/java/com/sparta/miniorder/MiniorderApplication.java
@@ -1,13 +1,13 @@
-package com.sparta.crud;
+package com.sparta.miniorder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CrudApplication {
+public class MiniorderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CrudApplication.class, args);
+        SpringApplication.run(MiniorderApplication.class, args);
     }
 
 }
